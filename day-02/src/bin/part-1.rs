6e49d22e69e4c6693a893f8450a6a1f0fb598336
Dgarc359 <dgarc359+github-personal@gmail.com>
2023-12-10 20:03:34 -0500

    fix texts and add skeleton for day 3

--- day-02/src/bin/part-1.rs
+++ day-02/src/bin/part-1.rs
@@ -89,7 +89,7 @@ impl GameConfig {
 mod tests {
     use super::*;
 
-    #[cfg(test)]
+    #[test]
     fn part_1() {
         let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
--- day-03/src/bin/part-1.rs
+++ day-03/src/bin/part-1.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 
 fn main() {
     let input = include_str!("./p1.txt");
@@ -5,22 +6,61 @@ fn main() {
     dbg!(output);
 }
 
-
 fn get_sum(input: &str) -> u32 {
     let sum = 0_u32;
 
-    for (i, c) in input.chars().enumerate() {
+    let symbol_coords = find_symbols(input);
 
-    }
-
+    let valid_nums = find_valid_nums(input, symbol_coords);
     sum
 }
 
+fn find_valid_nums(input: &str, symbol_coords: Vec<[usize; 2]>) -> Vec<usize> {
+    let re = &Regex::new(r"[0-9]").unwrap();
+    let mut valid_nums = vec![];
 
+    let rows = input.split(0xA as char);
+
+    let mut num_str = String::from("");
+
+    let mut num_has_adjacent_symbol = false;
+
+    for (y, row) in rows.enumerate() {
+        for (x, c) in row.chars().enumerate() {
+            if re.is_match(String::from(c).as_str()) {
+            }
+        }
+    }
+
+    valid_nums
+}
+
+fn check_cardinals_for_symbols(symbol_coords: Vec<[usize; 2]>, x: usize, y: usize) {
+
+}
+
+fn find_symbols(input: &str) -> Vec<[usize; 2]> {
+    let re = &Regex::new(r"[$+#*]").unwrap();
+    let mut coords = vec![];
+
+    let rows = input.split(0xA as char);
+
+    for (y, row) in rows.enumerate() {
+        for (x, c) in row.chars().enumerate() {
+            if re.is_match(String::from(c).as_str()) {
+                coords.push([x, y]);
+            }
+        }
+    }
+
+    coords
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
-    #[cfg(test)]
+    #[test]
     fn example_test() {
         let input = "467..114..
 ...*......
