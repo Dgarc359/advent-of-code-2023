b36ebf3d6b97efbc2d06c8d897583a4bb6746af0
Dgarc359 <dgarc359+github-personal@gmail.com>
2023-12-12 01:55:19 -0500

    refactor a bit and add approach for algorithm

--- day-03/src/bin/part-1.rs
+++ day-03/src/bin/part-1.rs
@@ -29,9 +29,18 @@ fn find_valid_nums(input: &str) -> Vec<usize> {
     let mut num_str = String::from("");
 
     for (y, row) in rows.enumerate() {
-
-            dbg!(&row);
+        dbg!(&row);
         for (x, c) in row.chars().enumerate() {
+            dbg!(c);
+            dbg!(has_adjacent_symbols(input, x, y, row.len()));
+
+            /*
+             * keep track of # as it's being read
+             * keep a separate flag that is whether or not that number is valid for the usm
+             * default is false.
+             * check cardinals and diagonals
+             * only check that flag and push to valid_nums when that flag has been set to true
+             */
             if has_adjacent_symbols(input, x, y, row.len()) {
                 num_str = String::from("");
                 continue;
@@ -43,10 +52,9 @@ fn find_valid_nums(input: &str) -> Vec<usize> {
             } else {
                 match num_str.as_str().parse::<usize>() {
                     Ok(i) => {
-
-                valid_nums.push(i);
+                        valid_nums.push(i);
                     }
-                    Err(_) => { }
+                    Err(_) => {}
                 }
 
                 num_str = String::from("");
@@ -64,43 +72,80 @@ fn has_adjacent_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool
 }
 
 fn cardinals_have_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool {
+    dbg!("checking cardinals", );
     // what is this wizardry? am I destructuring the array?? idk.
     // see reference: https://users.rust-lang.org/t/fast-removing-chars-from-string/24554
-    let filtered_input = input.replace(&['\n'][..],"");
+    let filtered_input = input.replace(&['\n'][..], "");
     let re = &Regex::new(r"[$+#*]").unwrap();
     let north = padding.saturating_mul(y.saturating_sub(1)) + x;
-    let south = (x + padding.saturating_mul(y.saturating_add(1))).clamp(0, input.len() -1 );
-    let east = x.saturating_sub(1) + padding.saturating_mul(y);
-    let west = x.saturating_add(1) + padding.saturating_mul(y).clamp(0, input.len() - 1);
+    let south = (padding
+        .saturating_mul(y.saturating_add(1))
+        .saturating_add(x)
+        .saturating_add(1))
+    .clamp(0, filtered_input.len() - 1);
+    let east = padding.saturating_mul(y).saturating_add(x).saturating_add(1).clamp(0, filtered_input.len() - 1);
+    let west =  padding.saturating_mul(y).saturating_add(x).saturating_sub(1).clamp(0, filtered_input.len() - 1);
 
-    dbg!(north, south, east, west, input);
-    let n_s = input.as_bytes()[north] as char;
-    let s_s = input.as_bytes()[south] as char;
-    let e_s = input.as_bytes()[east] as char;
-    let w_s = input.as_bytes()[west] as char;
+    dbg!(north, south, east, west, input, padding, &filtered_input);
+
+    let n_s: bool = match filtered_input.chars().nth(north) {
+        Some(i) => {
+            dbg!(north, &filtered_input.chars().nth(north));
+            re.is_match(String::from(i).as_str())
+        }
+        None => panic!("unable to index north symbol"),
+    };
+
+    let s_s: bool = match filtered_input.chars().nth(south) {
+        Some(i) => {
+            dbg!(south, &filtered_input.chars().nth(south));
+            re.is_match(String::from(i).as_str())
+        }
+        None => panic!("unable to index north symbol"),
+    };
+
+    let e_s = filtered_input.as_bytes()[east] as char;
+    let w_s = filtered_input.as_bytes()[west] as char;
 
     dbg!(n_s, s_s, e_s, w_s);
 
-    re.is_match(n_s.to_string().as_str())
-        || re.is_match(s_s.to_string().as_str())
-        || re.is_match(e_s.to_string().as_str())
-        || re.is_match(w_s.to_string().as_str())
+    n_s || s_s || re.is_match(e_s.to_string().as_str()) || re.is_match(w_s.to_string().as_str())
 }
 
 fn diagonals_have_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool {
-    let filtered_input = input.replace(&['\n'][..],"");
-let re = &Regex::new(r"[$+#*]").unwrap();
-    let north_east = padding.saturating_mul(y.saturating_sub(1)).saturating_add(x).saturating_add(1);
-    let north_west = padding.saturating_mul(y.saturating_sub(1)).saturating_add(x).saturating_sub(1);
-    let south_east = padding.saturating_mul(y.saturating_add(1)).saturating_add(x).saturating_add(1);
-    let south_west = padding.saturating_mul(y.saturating_add(1)).saturating_add(x).saturating_sub(1);
+    dbg!("checking diagonals");
+    let filtered_input = input.replace(&['\n'][..], "");
+    let re = &Regex::new(r"[$+#*]").unwrap();
+    let north_east = padding
+        .saturating_mul(y.saturating_sub(1))
+        .saturating_add(x)
+        .saturating_add(1);
+    let north_west = padding
+        .saturating_mul(y.saturating_sub(1))
+        .saturating_add(x)
+        .saturating_sub(1);
+    let south_east = padding
+        .saturating_mul(y.saturating_add(1))
+        .saturating_add(x)
+        .saturating_add(1).clamp(0, filtered_input.len() -1);
+    let south_west = padding
+        .saturating_mul(y.saturating_add(1))
+        .saturating_add(x)
+        .saturating_sub(1).clamp(0, filtered_input.len() - 1);
 
-    dbg!(north_east, north_west, south_east, south_west, filtered_input);
+    dbg!(
+        north_east,
+        north_west,
+        south_east,
+        south_west,
+        padding,
+        &filtered_input
+    );
 
-    let ne_s = input.as_bytes()[north_east] as char;
-    let nw_s = input.as_bytes()[north_west] as char;
-    let se_s = input.as_bytes()[south_east] as char;
-    let sw_s = input.as_bytes()[south_west] as char;
+    let ne_s = filtered_input.as_bytes()[north_east] as char;
+    let nw_s = filtered_input.as_bytes()[north_west] as char;
+    let se_s = filtered_input.as_bytes()[south_east] as char;
+    let sw_s = filtered_input.as_bytes()[south_west] as char;
 
     dbg!(ne_s, nw_s, se_s, sw_s);
 
@@ -108,7 +153,6 @@ let re = &Regex::new(r"[$+#*]").unwrap();
         || re.is_match(se_s.to_string().as_str())
         || re.is_match(nw_s.to_string().as_str())
         || re.is_match(sw_s.to_string().as_str())
-
 }
 
 #[cfg(test)]
