6116f6834d2a69f7c36c1050a39712656c0ec2da
Dgarc359 <dgarc359+github-personal@gmail.com>
2023-12-12 01:12:16 -0500

    filter input text

--- day-03/src/bin/part-1.rs
+++ day-03/src/bin/part-1.rs
@@ -6,12 +6,17 @@ fn main() {
     dbg!(output);
 }
 
-fn get_sum(input: &str) -> u32 {
-    let sum = 0_u32;
+fn get_sum(input: &str) -> usize {
+    let mut sum = 0_usize;
 
     //let symbol_coords = find_symbols(input);
 
     let valid_nums = find_valid_nums(input);
+
+    for num in valid_nums.iter() {
+        sum = sum.saturating_add(*num);
+    }
+
     sum
 }
 
@@ -24,6 +29,8 @@ fn find_valid_nums(input: &str) -> Vec<usize> {
     let mut num_str = String::from("");
 
     for (y, row) in rows.enumerate() {
+
+            dbg!(&row);
         for (x, c) in row.chars().enumerate() {
             if has_adjacent_symbols(input, x, y, row.len()) {
                 num_str = String::from("");
@@ -32,15 +39,22 @@ fn find_valid_nums(input: &str) -> Vec<usize> {
 
             if re.is_match(String::from(c).as_str()) {
                 num_str.push(c);
-                dbg!(&num_str);
+                dbg!(&num_str, &c);
             } else {
-                let num = num_str.as_str().parse::<usize>().unwrap();
-                valid_nums.push(num);
+                match num_str.as_str().parse::<usize>() {
+                    Ok(i) => {
+
+                valid_nums.push(i);
+                    }
+                    Err(_) => { }
+                }
 
                 num_str = String::from("");
             }
         }
     }
+
+    dbg!(&valid_nums);
 
     valid_nums
 }
@@ -50,18 +64,22 @@ fn has_adjacent_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool
 }
 
 fn cardinals_have_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool {
+    // what is this wizardry? am I destructuring the array?? idk.
+    // see reference: https://users.rust-lang.org/t/fast-removing-chars-from-string/24554
+    let filtered_input = input.replace(&['\n'][..],"");
     let re = &Regex::new(r"[$+#*]").unwrap();
     let north = padding.saturating_mul(y.saturating_sub(1)) + x;
-    let south = x + padding.saturating_mul(y.saturating_add(1));
+    let south = (x + padding.saturating_mul(y.saturating_add(1))).clamp(0, input.len() -1 );
     let east = x.saturating_sub(1) + padding.saturating_mul(y);
-    let west = x.saturating_add(1) + padding.saturating_mul(y);
+    let west = x.saturating_add(1) + padding.saturating_mul(y).clamp(0, input.len() - 1);
 
+    dbg!(north, south, east, west, input);
     let n_s = input.as_bytes()[north] as char;
     let s_s = input.as_bytes()[south] as char;
     let e_s = input.as_bytes()[east] as char;
     let w_s = input.as_bytes()[west] as char;
 
-    dbg!(north, south, east, west, n_s, s_s, e_s, w_s);
+    dbg!(n_s, s_s, e_s, w_s);
 
     re.is_match(n_s.to_string().as_str())
         || re.is_match(s_s.to_string().as_str())
@@ -70,11 +88,14 @@ fn cardinals_have_symbols(input: &str, x: usize, y: usize, padding: usize) -> bo
 }
 
 fn diagonals_have_symbols(input: &str, x: usize, y: usize, padding: usize) -> bool {
+    let filtered_input = input.replace(&['\n'][..],"");
 let re = &Regex::new(r"[$+#*]").unwrap();
     let north_east = padding.saturating_mul(y.saturating_sub(1)).saturating_add(x).saturating_add(1);
     let north_west = padding.saturating_mul(y.saturating_sub(1)).saturating_add(x).saturating_sub(1);
     let south_east = padding.saturating_mul(y.saturating_add(1)).saturating_add(x).saturating_add(1);
     let south_west = padding.saturating_mul(y.saturating_add(1)).saturating_add(x).saturating_sub(1);
+
+    dbg!(north_east, north_west, south_east, south_west, filtered_input);
 
     let ne_s = input.as_bytes()[north_east] as char;
     let nw_s = input.as_bytes()[north_west] as char;
@@ -109,6 +130,6 @@ mod tests {
 
         let output = get_sum(input);
 
-        assert_eq!(4361_u32, output);
+        assert_eq!(4361_usize, output);
     }
 }
